8278ffe05a9fae241a30a11c27c639f16d259d87
LucasLon22 <crlucaslondero@gmail.com>
2022-09-20 13:34:01 -0300

    first commit

---
+++ peliculasMovies.js
@@ -0,0 +1,100 @@
+peliculasMovie = [
+    {
+        "id": "Sonic The Hedgehog",
+        "titulo": "(2021)",
+        "duracion": "Calidad | Duración: 2:31:23",
+        "imagen": URL("./imagenes/srcBody/movie-1.png")
+    },
+    {
+        "id": "Morbius",
+        "titulo": "(2007)",
+        "duracion": "Calidad | Duración: 1:45:23",
+        "imagen": URL("./imagenes/srcBody/movie-2.png")
+    },
+    {
+        "id": "The Adam Project",
+        "titulo": "(2011)",
+        "duracion": "Calidad | Duración: 1:50:23",
+        "imagen": URL("./imagenes/srcBody/movie-3.png")
+    },
+    {
+        "id": "Free Guy",
+        "titulo": "(2006)",
+        "duracion":"Calidad | Duración: 1:30:23" ,
+        "imagen": URL("./imagenes/srcBody/movie-4.png")
+    },
+    {
+        "id":"Batman" ,
+        "titulo": "(1999)",
+        "duracion": "Calidad | Duración: 2:37:23",
+        "imagen": URL("./imagenes/srcBody/movie-5.png")
+    },
+    {
+        "id":"Uncharted" ,
+        "titulo": "(2004)",
+        "duracion":"Calidad | Duración: 1:45:23" ,
+        "imagen": URL("./imagenes/srcBody/movie-6.png")
+    },
+    {
+        "id": "Death on the Nile",
+        "titulo": "(2008)",
+        "duracion":"Calidad | Duración: 1:50:23" ,
+        "imagen": URL("./imagenes/srcBody/movie-7.png")
+    },
+    {
+        "id":"The King's Man" ,
+        "titulo": "(2001)",
+        "duracion": "Calidad | Duración: 1:30:23",
+        "imagen": URL("./imagenes/srcBody/movie-8.png")
+    },
+    {
+        "id": "Moon Knight",
+        "titulo": "(1997)",
+        "duracion": "Calidad | Duración: 2:317:23",
+        "imagen": URL("./imagenes/srcBody/series-1.png")
+    },
+    {
+        "id":"Halo" ,
+        "titulo":"(1977)" ,
+        "duracion":"Calidad | Duración: 1:45:23" ,
+        "imagen": URL("./imagenes/srcBody/series-2.png")
+    },
+    {
+        "id":"Vikings Valhalla" ,
+        "titulo": "(2007)",
+        "duracion":"Calidad | Duración: 1:50:23" ,
+        "imagen": URL("./imagenes/srcBody/series-3.png")
+    },
+    {
+        "id":"Money Heist" ,
+        "titulo": "(2005)",
+        "duracion": "Calidad | Duración: 1:30:23",
+        "imagen": URL("./imagenes/srcBody/series-4.png")
+    },
+    {
+        "id":"The Northman" ,
+        "titulo":"(2021)" ,
+        "duracion": "Calidad | Duración: 2:37:23",
+        "imagen": URL("./imagenes/srcBody/upcoming-1.png")
+    },
+    {
+        "id":"Doctor Strange<" ,
+        "titulo":"(1987)" ,
+        "duracion": "Calidad | Duración: 1:45:23",
+        "imagen": URL("./imagenes/srcBody/upcoming-2.png")
+    },
+    {
+        "id": "Memory",
+        "titulo":"(1985)" ,
+        "duracion": "Calidad | Duración: 1:50:23",
+        "imagen": URL("./imagenes/srcBody/upcoming-3.png")
+        
+    },
+    {
+        "id": "Massive Talent",
+        "titulo": "(1996)",
+        "duracion": "Calidad | Duración: 1:30:23",
+        "imagen": URL("./imagenes/srcBody/upcoming-4.png")
+    },
+
+]
