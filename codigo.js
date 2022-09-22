35a7dc20ea80e24cfe4297a4255aa5a190afe53d
LucasLon22 <crlucaslondero@gmail.com>
2022-09-22 10:29:01 -0300

    cards

--- codigo.js
+++ codigo.js
@@ -136,98 +136,98 @@ peliculasMovie = [
     {
         titulo: "Sonic The Hedgehog",
         Anio: "(2021)",
-        duracion: "Calidad | Duración: 2:31:23",
+        duracion: "2:31:23",
         imagen: "./imagenes/srcBody/movie-1.png"
     },
     {
         titulo: "Morbius",
         Anio: "(2007)",
-        duracion: "Calidad | Duración: 1:45:23",
+        duracion: "1:45:23",
         imagen: "./imagenes/srcBody/movie-2.png"
     },
     {
         titulo: "The Adam Project",
         Anio: "(2011)",
-        duracion: "Calidad | Duración: 1:50:23",
+        duracion: "1:50:23",
         imagen: "./imagenes/srcBody/movie-3.png"
     },
     {
         titulo: "Free Guy",
         Anio: "(2006)",
-        duracion: "Calidad | Duración: 1:30:23",
+        duracion: "1:30:23",
         imagen: "./imagenes/srcBody/movie-4.png"
     },
     {
         titulo: "Batman",
         Anio: "(1999)",
-        duracion: "Calidad | Duración: 2:37:23",
+        duracion: "2:37:23",
         imagen: "./imagenes/srcBody/movie-5.png"
     },
     {
         titulo: "Uncharted",
         Anio: "(2004)",
-        duracion: "Calidad | Duración: 1:45:23",
+        duracion: "1:45:23",
         imagen: "./imagenes/srcBody/movie-6.png"
     },
     {
         titulo: "Death on the Nile",
         AAnioño: "(2008)",
-        duracion: "Calidad | Duración: 1:50:23",
+        duracion: "1:50:23",
         imagen: "./imagenes/srcBody/movie-7.png"
     },
     {
         titulo: "The King's Man",
         Anio: "(2001)",
-        duracion: "Calidad | Duración: 1:30:23",
+        duracion: "1:30:23",
         imagen: "./imagenes/srcBody/movie-8.png"
     },
     {
         titulo: "Moon Knight",
         Anio: "(1997)",
-        duracion: "Calidad | Duración: 2:317:23",
+        duracion: "2:31:23",
         imagen: "./imagenes/srcBody/series-1.png"
     },
     {
         titulo: "Halo",
         Anio: "(1977)",
-        duracion: "Calidad | Duración: 1:45:23",
+        duracion: "1:45:23",
         imagen: "./imagenes/srcBody/series-2.png"
     },
     {
         titulo: "Vikings Valhalla",
         Anio: "(2007)",
-        duracion: "Calidad | Duración: 1:50:23",
+        duracion: "1:50:23",
         imagen: "./imagenes/srcBody/series-3.png"
     },
     {
         titulo: "Money Heist",
         Anio: "(2005)",
-        duracion: "Calidad | Duración: 1:30:23",
+        duracion: "1:30:23",
         imagen: "./imagenes/srcBody/series-4.png"
     },
     {
         titulo: "The Northman",
         Anio: "(2021)",
-        duracion: "Calidad | Duración: 2:37:23",
+        duracion: "2:37:23",
         imagen: "./imagenes/srcBody/upcoming-1.png"
     },
     {
         titulo: "Doctor Strange",
         Anio: "(1987)",
-        duracion: "Calidad | Duración: 1:45:23",
+        duracion: "1:45:23",
         imagen: "./imagenes/srcBody/upcoming-2.png"
     },
     {
         titulo: "Memory",
         Anio: "(1985)",
-        duracion: "Calidad | Duración: 1:50:23",
+        duracion: "1:50:23",
         imagen: "./imagenes/srcBody/upcoming-3.png"
 
     },
     {
         titulo: "Massive Talent",
         Anio: "(1996)",
-        duracion: "Calidad | Duración: 1:30:23",
+        duracion: "1:30:23",
         imagen: "./imagenes/srcBody/upcoming-4.png"
     },
 
@@ -265,8 +265,8 @@ botonBuscar.addEventListener('click', filtrar = () => {
             </div>
             <hr>
     
-            <div class="container">
-              <div class="caracteristicas row align-items-start"></div>
+            <div class="container row">
+              <div class="align-items-start"></div>
               <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
                 class="bi bi-badge-4k-fill mx-3 mb-4 col color-icono" viewBox="0 0 16 16">
                 <path d="M3.577 8.9v.03h1.828V5.898h-.062a46.781 46.781 0 0 0-1.766 3.001z" />
@@ -279,7 +279,7 @@ botonBuscar.addEventListener('click', filtrar = () => {
                 <path
                   d="M5.667 16C4.747 16 4 15.254 4 14.333v-1.86A5.985 5.985 0 0 1 2 8c0-1.777.772-3.374 2-4.472V1.667C4 .747 4.746 0 5.667 0h4.666C11.253 0 12 .746 12 1.667v1.86a5.99 5.99 0 0 1 1.918 3.48.502.502 0 0 1 .582.493v1a.5.5 0 0 1-.582.493A5.99 5.99 0 0 1 12 12.473v1.86c0 .92-.746 1.667-1.667 1.667H5.667zM13 8A5 5 0 1 0 3 8a5 5 0 0 0 10 0z" />
               </svg>
-              <p class="mx-2 col texto-descripcion ">${pelicula.duracion}</p>
+              <p class="col text-light p-1">${pelicula.duracion}</p>
             </div>
           </div>
         </div>
